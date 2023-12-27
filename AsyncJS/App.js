b4e6b09e459236c49845077699b008b20298691c
Vikash Singh <vik439@gmail.com>
2023-12-27 14:05:51 +0530

    Create App.js

---
+++ AsyncJS/App.js
@@ -0,0 +1,13 @@
+// App.js
+import React from 'react';
+import User from './User';
+
+function App() {
+  return (
+    <div className="App">
+      <User />
+    </div>
+  );
+}
+
+export default App;
